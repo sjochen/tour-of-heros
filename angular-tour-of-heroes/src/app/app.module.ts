6bafa918d26b0fecf37b03d5c3cc998866968457
Stephen Jochen <sjochen@PMIDSD.com>
2020-08-06 11:23:47 -0400

    added basic routing and link so user can route to heroes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
    declarations: [
@@ -16,7 +18,8 @@ import { MessagesComponent } from './messages/messages.component';
    ],
    imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      AppRoutingModule
    ],
    providers: [],
    bootstrap: [
